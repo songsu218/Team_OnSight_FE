f0f7d78f6849ca71b9ac8ec11834b1dab32046f8
ryukh1003 <162939237+ryukh1003@users.noreply.github.com>
2024-06-11 15:58:32 +0900

    Revert "Feater gnb1 헤더 만들기"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './css/my_reset.css';
 import './css/App.css';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
