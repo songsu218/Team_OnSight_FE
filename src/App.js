89f404550d0f2b8914dba18f8d770402025244b5
jubi <joobe1031@gmail.com>
2024-06-12 11:04:36 +0900

    feat: 로그인

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import './css/my_reset.css';
 import './css/App.css';
-
 import Header from './components/Header';
-import Footer from './components/Footer';
+import SignInPage from './pages/SignInPage';
 
 function App() {
   return (
     <div>
       <Header />
-      <Footer />
+      <SignInPage />
     </div>
   );
 }
