8fe54443cfb2826ac6e8bea1b386012d33b77446
jubi <joobe1031@gmail.com>
2024-06-12 11:23:39 +0900

    feat: login-app.js수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './css/my_reset.css';
 import './css/App.css';
 import Header from './components/Header';
-import SignInPage from './pages/SignInPage';
 
 function App() {
   return (
     <div>
       <Header />
-      <SignInPage />
     </div>
   );
 }
