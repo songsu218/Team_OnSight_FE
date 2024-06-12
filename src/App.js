5a473e67d8a014998ce2995d312238aed9041b87
kyuhwan <konge2963@gmail.com>
2024-06-12 13:25:46 +0900

    feat : main은 만드는 중 mw같은 공통사항 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './css/my_reset.css';
 import './css/App.css';
 
 import Header from './components/Header';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,57 @@
+import style from '../css/Main.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+
+import RecordList from '../components/RecordList';
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main className={`${style.main} mw`}>
+      <div className={style.recordArea}>
+        <section>
+          <Swiper
+            slidesPerView={3}
+            loop={true}
+            spaceBetween={30}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <RecordList />
+            </SwiperSlide>
+            <SwiperSlide>
+              <RecordList />
+            </SwiperSlide>
+            <SwiperSlide>
+              <RecordList />
+            </SwiperSlide>
+            <SwiperSlide>
+              <RecordList />
+            </SwiperSlide>
+            <SwiperSlide>
+              <RecordList />
+            </SwiperSlide>
+          </Swiper>
+        </section>
+      </div>
+      <div className={style.cupBox}>
+        <div className={style.textBox}>
+          <p>무언가 들어갈 자리</p>
+        </div>
+        <div className={style.cupArea}></div>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
