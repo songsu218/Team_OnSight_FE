8e06706eec943def098aa409f799c5cfc2dd7f38
songsu218 <chaengleee@naver.com>
2024-06-14 16:05:11 +0900

    <fix> mui 추가

--- src/components/challenge/ChallengeBox.jsx
+++ src/components/challenge/ChallengeBox.jsx
@@ -1,58 +1,114 @@
+// import { Box } from "@mui/material";
 import * as React from "react";
-// import "../css/ChallengeBox.css";
-import { Autoplay } from "swiper/modules";
-import ChallengeList from "./ChallengeList";
-import { Swiper, SwiperSlide } from "swiper/react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
+import Card from "@mui/joy/Card";
+import { Button, Grid } from "@mui/material";
 const ChallengeBox = (props) => {
-  const {subjectList , detailList, srcList } = { ...props,};
+  const { subjectList, detailList, srcList , isVisible  } = { ...props };
 
-  const subject1 = '경기도 어찌구 챌린지';
-  const subject2 = '서울 어찌구 챌린지';
-  const subject3 = '강원도 어찌구 챌린지';
-  const subject4 = '전라도 어찌구 챌린지';
-  const detail = '현재1등누구세요';
-  const src1 = "/img/gyeonggi.png"
-  const src2 = "/img/seoul.png"
-  const src3 = "/img/gangwon.png"
-  const src4 = "/img/jeonra.png"
-  
+  const data = [
+    {
+      src: "/img/gyeonggi.png",
+      title: "경기도 어찌구 챌린지",
+      description: "경기 클라이밍장 이름",
+    },
+    {
+      src: "/img/seoul.png",
+      title: "서울 어찌구 챌린지",
+      description: "서울 클라이밍장 이름",
+    },
+    {
+      src: "/img/gangwon.png",
+      title: "강원도 어찌구 챌린지",
+      description: "강원 클라이밍장 이름",
+    },
+  ];
+
   return (
-    <div>
-      <section>
-        <Swiper
-          slidesPerView={2}
-          loop={true}
-          spaceBetween={30}
-          breakpoints={{
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            980: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            1350: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-          }}
-          autoplay={{
-            delay: 4500,
-            disableOnInteraction: false,
-          }}
-          modules={[Autoplay]}
-          className="mySwiper"
+    <Box
+      sx={{
+        display: "flex",
+        gap: 5,
+        py: 1,
+        overflow: "auto",
+        width: "auto",
+        minHeight: "140px",
+        scrollSnapType: "x mandatory",
+        "& > *": {
+          scrollSnapAlign: "center",
+        },
+        "::-webkit-scrollbar": { display: "none" },
+      }}
+    >
+      {data.map((item) => (
+        <Card
+          orientation="horizontal"
+          size="sm"
+          key={item.title}
+          variant="outlined"
+          sx={{ borderRadius: "1rem" }}
         >
-          <SwiperSlide>
-            <ChallengeList subject = {subject1} src = {src1} detail = {detail}/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ChallengeList subject = {subject2} src = {src2} detail = {detail}/>
-          </SwiperSlide>
-        </Swiper>
-      </section>
-    </div>
+          <Box sx={{ whiteSpace: "nowrap", mx: 1 }}>
+            <Typography
+              sx={{
+                fontFamily: "Roboto",
+                fontSize: "26px",
+                fontWeight: 700,
+                lineHeight: "35.2px",
+                textAlign: "left",
+              }}
+            >
+              {item.title}
+            </Typography>
+            <Typography
+              level="body-sm"
+              sx={{
+                fontFamily: "Roboto",
+                fontStyle: "nomal",
+                fontWeight: 700,
+                textAlign: "left",
+              }}
+            >
+              {item.description}
+            </Typography>
+          </Box>
+          <AspectRatio ratio="1" sx={{ minHeight: 120, minWidth: 120 }}>
+            <img
+              srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.src}?h=120&fit=crop&auto=format`}
+              alt={item.title}
+            />
+          </AspectRatio>
+          <Grid container sx={{ display: "flex", alignItems: "end" }}>
+            <Grid item sx={{}}>
+              { isVisible ? 
+              <Button
+                variant="contained"
+                sx={{
+                  height: "60px",
+                  width: "100%",
+                  borderRadius: "50%",
+                  fontFamily: "Roboto",
+                  fontStyle: "normal",
+                  fontWeight: 700,
+                  fontSize: '18px',
+                  color: "#000000",
+                  backgroundColor: "#d3d3d3",
+                  textTransform: "none",
+                  padding: 0,
+                }}
+              >
+                참가
+              </Button>
+              : ''
+              }
+            </Grid>
+          </Grid>
+        </Card>
+      ))}
+    </Box>
   );
 };
 
--- src/pages/Challenge.jsx
+++ src/pages/Challenge.jsx
@@ -1,48 +1,63 @@
 import React, { useState, useEffect } from "react";
 
 import style from "../css/Challenge.module.css";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Autoplay } from "swiper/modules";
-import RecordList from "../components/RecordList";
 import ChallengeBox from "../components/challenge/ChallengeBox";
+import { Button, Container, Grid, Typography } from "@mui/material";
 
 const Challenge = () => {
+  const line = { border: "1px solid red" };
   return (
-    <main className={`${style.mw} ${style.container}`}>
-      <div className={style.top_section}>
-        <div className={`${style.inner_container}`}>
-          
-          <div className={style.inner_top}>
-            <div className={`${style.subject}`}>진행 중인 챌린지에요</div>
-            <button className={`${style.myCh}`}>나의 챌린지 {">"} </button>
-          </div>
-          <div className={style.inner_bottom}>
-            <button className={style.leftButton} />
-            <div className={style.inner_bottom_item}>
-              <ChallengeBox></ChallengeBox>
-            </div>
-            <button className={style.rightButton} />
-          </div>
-        상단
-        </div>
-      </div>
-      <div className={style.bottom_section}>
-        <div className={`${style.inner_container}`}>
-          하단
-          <div className={style.inner_top}>
-            <div className={`${style.subject}`}>지난 챌린지에요</div>
-          </div>
-          <div className={style.inner_bottom}>
-            <button className={style.leftButton} />
-            <div className={style.inner_bottom_item}>
-              <ChallengeBox></ChallengeBox>
-            </div>
-            <button className={style.rightButton} />
-          </div>
-        </div>
-      </div>
-    </main>
+    <Container sx={{ border: "1px solid red", width: '100%'}}>
+      <Grid Container display="flex" sx={{height: '50vh' , width: '100%', borderBottom: '1px solid black'}}>
+        <Grid item sx={line}>
+          <Typography>진행 중인 챌린지에요</Typography>
+        </Grid>
+        <Grid item xs></Grid>
+        <Grid item sx={line}>
+          <Button> 나의 챌린지 {">"}</Button>
+        </Grid>
+      </Grid>
+      <Grid Container sx={{height: '50vh'}}>
+        <Grid item xs={12} sx={line}>
+          <Typography>지난 챌린지에요</Typography>
+        </Grid>
+      </Grid>
+    </Container>
+    // <main className={`${style.mw} ${style.container}`}>
+    //   <div className={style.top_section}>
+    //     <div className={`${style.inner_container}`}>
+
+    //       <div className={style.inner_top}>
+    //         <div className={`${style.subject}`}>진행 중인 챌린지에요</div>
+    //         <button className={`${style.myCh}`}>나의 챌린지 {">"} </button>
+    //       </div>
+    //       <div className={style.inner_bottom}>
+    //         <button className={style.leftButton} />
+    //         <div className={style.inner_bottom_item}>
+    //           <ChallengeBox isVisible = {true}></ChallengeBox>
+    //         </div>
+    //         <button className={style.rightButton} />
+    //       </div>
+    //     상단
+    //     </div>
+    //   </div>
+    //   <div className={style.bottom_section}>
+    //     <div className={`${style.inner_container}`}>
+    //       하단
+    //       <div className={style.inner_top}>
+    //         <div className={`${style.subject}`}>지난 챌린지에요</div>
+    //       </div>
+    //       <div className={style.inner_bottom}>
+    //         <button className={style.leftButton} />
+    //         <div className={style.inner_bottom_item}>
+    //           <ChallengeBox></ChallengeBox>
+    //         </div>
+    //         <button className={style.rightButton} />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
   );
 };
 export default Challenge;
