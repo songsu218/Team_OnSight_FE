452e6b514379b6110f1f77715e46a1d9322474ca
songsu218 <asane7@naver.com>
2024-06-14 13:55:12 +0900

    feat: mypage view 작업

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,62 +1,59 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import style from "../css/SignUpPage.module.css";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import style from '../css/SignUpPage.module.css';
 
 const SignUpPage = () => {
-  const [id, setId] = useState("");
-  const [nick, setNick] = useState("");
-  const [password, setPassword] = useState("");
-  const [pdcon, setPdcon] = useState("");
-  const [message1, setMessage1] = useState("");
-  const [message2, setMessage2] = useState("");
-  const [message3, setMessage3] = useState("");
-  const [message4, setMessage4] = useState("");
+  const [id, setId] = useState('');
+  const [nick, setNick] = useState('');
+  const [password, setPassword] = useState('');
+  const [pdcon, setPdcon] = useState('');
+  const [message1, setMessage1] = useState('');
+  const [message2, setMessage2] = useState('');
+  const [message3, setMessage3] = useState('');
+  const [message4, setMessage4] = useState('');
 
   const register = async (e) => {
     e.preventDefault();
     console.log(id);
     if (!/^[a-zA-Z0-9_-]{4,10}$/.test(id)) {
-      setMessage1("4~10자 내외 영문,숫자,특수기호,_,-만 사용 가능합니다.");
+      setMessage1('4~10자 내외 영문,숫자,특수기호,_,-만 사용 가능합니다.');
       return;
     } else {
-      setMessage1("");
+      setMessage1('');
     }
     if (!/^[a-zA-Z0-9가-힣\u4E00-\u9FFF_-]{1,10}$/.test(nick)) {
-      setMessage2("닉네임은 10자 이내로 입력하세요.");
+      setMessage2('닉네임은 10자 이내로 입력하세요.');
       return;
     } else {
-      setMessage2("");
+      setMessage2('');
     }
-    if (
-      !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-        password
-      )
-    ) {
-      setMessage3("영문,숫자,특수문자를 포함한 8자이상 입력해주세요");
+    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)) {
+      setMessage3('영문,숫자,특수문자를 포함한 8자이상 입력해주세요');
       return;
     } else {
-      setMessage3("");
+      setMessage3('');
     }
     if (password !== pdcon) {
-      setMessage4("동일한 패스워드를 입력하세요");
+      setMessage4('동일한 패스워드를 입력하세요');
       return;
     } else {
-      setMessage4("");
+      setMessage4('');
     }
 
     //백엔드로 POST 요청 및 응답
-    const response = await fetch("http://localhost:8000/user/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch('http://localhost:8000/user/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ id, nick, password }),
     });
+    console.log(response);
     console.log(await response.json());
 
     if (response.status === 200) {
       //회원가입완료
       // window.location.href = "/SignInPage";
     } else {
-      alert("이미 존재하는 아이디 입니다.");
+      alert('이미 존재하는 아이디 입니다.');
     }
   };
 
