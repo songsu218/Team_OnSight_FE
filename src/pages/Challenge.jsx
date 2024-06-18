91583c1f68cdb9033aac908325e39fedd3b7a185
songsu218 <chaengleee@naver.com>
2024-06-18 18:03:56 +0900

    feat : 중간작업점 저장

--- src/pages/Challenge.jsx
+++ src/pages/Challenge.jsx
@@ -1,204 +1,204 @@
-import React, { useState, useEffect, useRef } from "react";
-
+import style from "../css/challenge.module.css";
+import React from "react";
 import "swiper/css";
-import ChallengeBox from "../components/challenge/ChallengeBox";
-import { Button, Container, Grid, IconButton, Typography } from "@mui/material";
-import ControlPointOutlinedIcon from "@mui/icons-material/ControlPointOutlined";
-import { ch } from "../api";
 
 const Challenge = () => {
-  const [ongoingChallenges, setOngoingChallenges] = useState([]);
-  const [pastChallenges, setPastChallenges] = useState([]);
-  const test = () => {
-    ch.chCurrentList(new Date())
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
-  };
-
-  const data = [
-    {
-      id: 1,
-      title: "경기도 어찌구 챌린지",
-      center: "경기 클라이밍장 이름",
-      date: "20240714",
-      images: "/img/gyeonggi.png",
-    },
-    {
-      id: 2,
-      title: "서울 어찌구 챌린지",
-      center: "서울 클라이밍장 이름",
-      date: "20240715",
-      images: "/img/seoul.png",
-    },
-    {
-      id: 3,
-      title: "경기2 어찌구 챌린지",
-      center: "경기2 클라이밍장 이름",
-      date: "20240715",
-      images: "/img/gyeonggi.png",
-    },
-    {
-      id: 4,
-      title: "서울2 어찌구 챌린지",
-      center: "서울2 클라이밍장 이름",
-      date: "20240715",
-      images: "/img/seoul.png",
-    },
-    {
-      id: 5,
-      images: "/img/gangwon.png",
-      title: "강원도 어찌구 챌린지",
-      date: "20240519",
-      center: "강원 클라이밍장 이름",
-    },
-    {
-      id: 6,
-      images: "/img/gangwon.png",
-      title: "강원도2 어찌구 챌린지",
-      date: "20240519",
-      center: "강원2 클라이밍장 이름",
-    },
-    {
-      id: 7,
-      images: "/img/gangwon.png",
-      title: "강원도3 어찌구 챌린지",
-      date: "20240519",
-      center: "강원3 클라이밍장 이름",
-    },
-  ];
-
-  useEffect(() => {
-    const today = new Date();
-    const formattedToday = `${today.getFullYear()}${(
-      "0" +
-      (today.getMonth() + 1)
-    ).slice(-2)}${("0" + today.getDate()).slice(-2)}`;
-
-    const ongoing = [];
-    const past = [];
-
-    data.forEach((challenge) => {
-      if (challenge.date > formattedToday) {
-        ongoing.push(challenge);
-      } else {
-        past.push(challenge);
-      }
-    });
-
-    setOngoingChallenges(ongoing);
-    setPastChallenges(past);
-  }, []);
   return (
     <>
-      <Grid
-        container
-        sx={{
-          height: "50vh",
-          borderBottom: "1px solid black",
-        }}
-      >
-        <Grid item xs={12} sx={{ display: "flex", marginTop: "15vh" }}>
-          <Grid item xs={4} sx={{ display: "flex" }}>
-            <Grid item xs={3.5}></Grid>
-            <Grid item xs>
-              <Grid item xs={12} sx={{}}>
-                <Typography
-                  sx={{
-                    fontFamily: "Roboto",
-                    fontStyle: "normal",
-                    fontWeight: "400",
-                    fontSize: "2.5rem",
-                  }}
-                >
-                  진행 중인 챌린지에요
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs></Grid>
-          <Grid item xs={2} sx={{ display: "flex" }}>
-            <Grid item xs={12} sx={{}}>
-              <Grid item xs={3.5}></Grid>
-              <Button
-                sx={{
-                  fontfamily: "Roboto",
-                  fontstyle: "normal",
-                  fontsize: "2rem",
-                  color: "#000000",
-                }}
-              >
-                {" "}
-                나의 챌린지 {">"}
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12} sx={{ display: "flex", alignItems: "center" }}>
-          <Grid item xs>
-            <ChallengeBox
-              showJoinButton={true}
-              slidesPerViewCount={2}
-              dataList={ongoingChallenges}
-            ></ChallengeBox>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        sx={{
-          height: "50vh",
-          // paddingBottom:'1vh',
-        }}
-      >
-        <Grid item xs={12} sx={{ display: "flex", marginTop: "2vh" }}>
-          <Grid item xs={4} sx={{ display: "flex" }}>
-            <Grid item xs={3.5}></Grid>
-            <Grid item xs>
-              <Typography
-                sx={{
-                  fontFamily: "Roboto",
-                  fontStyle: "normal",
-                  fontWeight: "400",
-                  fontSize: "2.5rem",
-                }}
-              >
-                지난 챌린지에요
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs></Grid>
-          <Grid item xs={2} sx={{ display: "flex" }}>
-            <Grid item xs={12} sx={{}}>
-              <Grid item xs={3.5}></Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12} sx={{ display: "flex", alignItems: "center" }}>
-          <Grid item xs>
-            <ChallengeBox
-              showJoinButton={false}
-              slidesPerViewCount={2}
-              dataList={pastChallenges}
-            ></ChallengeBox>
-          </Grid>
-        </Grid>
-        <Grid item xs={12} sx={{ display: "flex" }}>
-          <Grid item xs></Grid>
-          <Grid item xs={1}>
-            <IconButton
-              aria-label="+"
-              sx={{ width: "70px", height: "70px" }}
-              onClick={() => {}}
-            >
-              <ControlPointOutlinedIcon sx={{ fontSize: "3rem" }} />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </Grid>
+      <>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <style
+          dangerouslySetInnerHTML={{
+            __html:
+              ".page-tit-area {\n      display: flex;\n      justify-content: space-between;\n      align-items: center;\n      margin: 0 0 5.6rem;\n    }",
+          }}
+        />
+        <title>Document</title>
+        {/* <link
+          rel="stylesheet"
+          href={style}
+        /> */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+        />
+
+        <main id={style.container}>
+          <div className={style.content}>
+            {/* h1은 로고(사이트명)이라 가정하고 */}
+            <div className="page-tit-area">
+              <h2 className="page-tit">챌린지 일정</h2>
+              <nav className="page-nav">
+                <ul>
+                  <li className="active">
+                    <a
+                      href="#"
+                      className="page-link"
+                      title="나의 챌린지 만들기 페이지 이동 링크"
+                    >
+                      나의 챌린지 만들기
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="page-link"
+                      title="나의 챌린지 보기 페이지 이동 링크"
+                    >
+                      나의 챌린지 보기
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="sub-tit-area">
+              <h3 className="sub-tit">2024</h3>
+            </div>
+            <div className="filter-option">
+              <label htmlFor="challengeOption1" className="option-label">
+                <span>상태</span>
+              </label>
+              <select id="challengeOption1" className="option-list">
+                <option>선택</option>
+                <option value={1}>진행 중</option>
+                <option value={2}>진행 끝</option>
+              </select>
+            </div>
+            <div className="challenge-wrap">
+              <div className="swiper mySwiper">
+                <div className="swiper-wrapper">
+                  <div className="swiper-slide">
+                    <a
+                      href="https://www.naver.com"
+                      title="해당 챌린지 상세 페이지로 이동"
+                      className="challenge-link"
+                      target="_blank"
+                    >
+                      <div className="challenge-img">
+                        <img
+                          src="https://ojsfile.ohmynews.com/PHT_IMG_FILE/2023/0525/IE003155586_PHT.jpg"
+                          alt="A챌린지"
+                        />
+                      </div>
+                      <span className="challenge-cate">클라이밍장 이름</span>
+                      <em className="challenge-tit">C챌린지</em>
+                      <span className="challenge-txt">
+                        2024.06.20(목) ~ 2024.06.23(일)
+                        <p />
+                      </span>
+                    </a>
+                  </div>
+                  <div className="swiper-slide">
+                    <a
+                      href="#"
+                      title="해당 챌린지 상세 페이지로 이동"
+                      className="challenge-link"
+                    >
+                      <div className="challenge-img">
+                        <img
+                          src="https://mblogthumb-phinf.pstatic.net/MjAxODExMDFfODMg/MDAxNTQxMDQ5NTQ4MDM1.HTE_ClokpSF4eWutgUkimC8L8qU4WdgCayAfSDfY1NQg.Vv6m5UgI_1kXKBIgrdRIlznAyTh3ng9mGMtAv8Ccdcog.JPEG.jung_nang_gu/%ED%99%8D%EB%B3%B4%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg?type=w800"
+                          alt="A챌린지"
+                        />
+                      </div>
+                      <span className="challenge-cate">연극</span>
+                      <em className="challenge-tit">B챌린지B챌린지B챌린지</em>
+                      <span className="challenge-txt">
+                        2024.06.20(목) ~ 2024.06.23(일)
+                        <p />
+                      </span>
+                    </a>
+                  </div>
+                  <div className={'swiper-slide'}> 
+                    <a
+                      href="#"
+                      title="해당 챌린지 상세 페이지로 이동"
+                      className="challenge-link"
+                    >
+                      <div className="challenge-img">
+                        <img
+                          src="https://ojsfile.ohmynews.com/PHT_IMG_FILE/2023/0525/IE003155586_PHT.jpg"
+                          alt="A챌린지"
+                        />
+                      </div>
+                      <span className="challenge-cate">연극 이름</span>
+                      <em className="challenge-tit">A챌린지</em>
+                      <span className="challenge-txt">
+                        2024.06.20(목) ~ 2024.06.23(일)
+                        <p />
+                      </span>
+                    </a>
+                  </div>
+                  <div className="swiper-slide">
+                    <a
+                      href="#"
+                      title="해당 챌린지 상세 페이지로 이동"
+                      className="challenge-link"
+                    >
+                      <div className="challenge-img">
+                        <img
+                          src="https://ojsfile.ohmynews.com/PHT_IMG_FILE/2023/0525/IE003155586_PHT.jpg"
+                          alt="A챌린지"
+                        />
+                      </div>
+                      <span className="challenge-cate">클라이밍장 이름</span>
+                      <em className="challenge-tit">A챌린지</em>
+                      <span className="challenge-txt">
+                        2024.06.20(목) ~ 2024.06.23(일)
+                        <p />
+                      </span>
+                    </a>
+                  </div>
+                  <div className="swiper-slide">
+                    <a
+                      href="#"
+                      title="해당 챌린지 상세 페이지로 이동"
+                      className="challenge-link"
+                    >
+                      <div className="challenge-img">
+                        <img
+                          src="https://ojsfile.ohmynews.com/PHT_IMG_FILE/2023/0525/IE003155586_PHT.jpg"
+                          alt="A챌린지"
+                        />
+                      </div>
+                      <span className="challenge-cate">클라이밍장 이름</span>
+                      <em className="challenge-tit">A챌린지</em>
+                      <span className="challenge-txt">
+                        2024.06.20(목) ~ 2024.06.23(일)
+                        <p />
+                      </span>
+                    </a>
+                  </div>
+                </div>
+                <div className="control">
+                  <div className="swiper-button-prev" />
+                  <div className="swiper-pagination" />
+                  <div className="autoplay-progress">
+                    <svg viewBox="0 0 48 48">
+                      <circle cx={24} cy={24} r={20} />
+                    </svg>
+                    <button
+                      type="button"
+                      className="control-btn btn-stop"
+                      title="정지 버튼"
+                    >
+                      자동재생 정지 버튼
+                    </button>
+                    <button
+                      type="button"
+                      className="control-btn btn-play"
+                      title="재생 버튼"
+                    >
+                      재생 버튼
+                    </button>
+                  </div>
+                  <div className="swiper-button-next" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </>
     </>
   );
 };
---
+++ src/pages/Challenge_backup.jsx
@@ -0,0 +1,205 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import "swiper/css";
+import ChallengeBox from "../components/challenge/ChallengeBox";
+import { Button, Container, Grid, IconButton, Typography } from "@mui/material";
+import ControlPointOutlinedIcon from "@mui/icons-material/ControlPointOutlined";
+import { ch } from "../api";
+
+const Challenge_backup = () => {
+  const [ongoingChallenges, setOngoingChallenges] = useState([]);
+  const [pastChallenges, setPastChallenges] = useState([]);
+  const test = () => {
+    ch.chCurrentList(new Date())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(`${error}`);
+      });
+  };
+
+  const data = [
+    {
+      id: 1,
+      title: "경기도 어찌구 챌린지",
+      center: "경기 클라이밍장 이름",
+      date: "20240714",
+      images: "/img/gyeonggi.png",
+    },
+    {
+      id: 2,
+      title: "서울 어찌구 챌린지",
+      center: "서울 클라이밍장 이름",
+      date: "20240715",
+      images: "/img/seoul.png",
+    },
+    {
+      id: 3,
+      title: "경기2 어찌구 챌린지",
+      center: "경기2 클라이밍장 이름",
+      date: "20240715",
+      images: "/img/gyeonggi.png",
+    },
+    {
+      id: 4,
+      title: "서울2 어찌구 챌린지",
+      center: "서울2 클라이밍장 이름",
+      date: "20240715",
+      images: "/img/seoul.png",
+    },
+    {
+      id: 5,
+      images: "/img/gangwon.png",
+      title: "강원도 어찌구 챌린지",
+      date: "20240519",
+      center: "강원 클라이밍장 이름",
+    },
+    {
+      id: 6,
+      images: "/img/gangwon.png",
+      title: "강원도2 어찌구 챌린지",
+      date: "20240519",
+      center: "강원2 클라이밍장 이름",
+    },
+    {
+      id: 7,
+      images: "/img/gangwon.png",
+      title: "강원도3 어찌구 챌린지",
+      date: "20240519",
+      center: "강원3 클라이밍장 이름",
+    },
+  ];
+
+  useEffect(() => {
+    const today = new Date();
+    const formattedToday = `${today.getFullYear()}${(
+      "0" +
+      (today.getMonth() + 1)
+    ).slice(-2)}${("0" + today.getDate()).slice(-2)}`;
+
+    const ongoing = [];
+    const past = [];
+
+    data.forEach((challenge) => {
+      if (challenge.date > formattedToday) {
+        ongoing.push(challenge);
+      } else {
+        past.push(challenge);
+      }
+    });
+
+    setOngoingChallenges(ongoing);
+    setPastChallenges(past);
+  }, []);
+  return (
+    <>
+      <Grid
+        container
+        sx={{
+          height: "50vh",
+          borderBottom: "1px solid black",
+        }}
+      >
+        <Grid item xs={12} sx={{ display: "flex", marginTop: "15vh" }}>
+          <Grid item xs={4} sx={{ display: "flex" }}>
+            <Grid item xs={3.5}></Grid>
+            <Grid item xs>
+              <Grid item xs={12} sx={{}}>
+                <Typography
+                  sx={{
+                    fontFamily: "Roboto",
+                    fontStyle: "normal",
+                    fontWeight: "400",
+                    fontSize: "2.5rem",
+                  }}
+                >
+                  진행 중인 챌린지에요
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs></Grid>
+          <Grid item xs={2} sx={{ display: "flex" }}>
+            <Grid item xs={12} sx={{}}>
+              <Grid item xs={3.5}></Grid>
+              <Button
+                sx={{
+                  fontfamily: "Roboto",
+                  fontstyle: "normal",
+                  fontsize: "2rem",
+                  color: "#000000",
+                }}
+              >
+                {" "}
+                나의 챌린지 {">"}
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ display: "flex", alignItems: "center" }}>
+          <Grid item xs>
+            <ChallengeBox
+              showJoinButton={true}
+              slidesPerViewCount={2}
+              dataList={ongoingChallenges}
+            ></ChallengeBox>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          height: "50vh",
+          // paddingBottom:'1vh',
+        }}
+      >
+        <Grid item xs={12} sx={{ display: "flex", marginTop: "2vh" }}>
+          <Grid item xs={4} sx={{ display: "flex" }}>
+            <Grid item xs={3.5}></Grid>
+            <Grid item xs>
+              <Typography
+                sx={{
+                  fontFamily: "Roboto",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  fontSize: "2.5rem",
+                }}
+              >
+                지난 챌린지에요
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs></Grid>
+          <Grid item xs={2} sx={{ display: "flex" }}>
+            <Grid item xs={12} sx={{}}>
+              <Grid item xs={3.5}></Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ display: "flex", alignItems: "center" }}>
+          <Grid item xs>
+            <ChallengeBox
+              showJoinButton={false}
+              slidesPerViewCount={2}
+              dataList={pastChallenges}
+            ></ChallengeBox>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ display: "flex" }}>
+          <Grid item xs></Grid>
+          <Grid item xs={1}>
+            <IconButton
+              aria-label="+"
+              sx={{ width: "70px", height: "70px" }}
+              onClick={() => {}}
+            >
+              <ControlPointOutlinedIcon sx={{ fontSize: "3rem" }} />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default Challenge_backup;
