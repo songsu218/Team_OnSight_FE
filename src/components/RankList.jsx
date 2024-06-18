766ba52c3c019a0abc877a1340b6869d5fc01441
kyuhwan <konge2963@gmail.com>
2024-06-18 19:45:33 +0900

    feat: main 껍데기는 완성

--- src/components/RankList.jsx
+++ src/components/RankList.jsx
@@ -1,48 +1,23 @@
 import style from '../css/RankList.module.css';
 
-const RankList = () => {
+const RankList = ({ hoveredCard }) => {
   return (
     <ul className={style.rankListCon}>
-      <li className={style.rankListCard}>
-        <strong>1</strong>
-        <span>
-          <img src="/img/test.jpg" alt="profile" />
-          <strong>닉네임</strong>
-        </span>
-        <span>5,000</span>
-      </li>
-      <li className={style.rankListCard}>
-        <strong>2</strong>
-        <span>
-          <img src="/img/test.jpg" alt="profile" />
-          <strong>닉네임</strong>
-        </span>
-        <span>5,000</span>
-      </li>
-      <li className={style.rankListCard}>
-        <strong>3</strong>
-        <span>
-          <img src="/img/test.jpg" alt="profile" />
-          <strong>닉네임</strong>
-        </span>
-        <span>5,000</span>
-      </li>
-      <li className={style.rankListCard}>
-        <strong>4</strong>
-        <span>
-          <img src="/img/test.jpg" alt="profile" />
-          <strong>닉네임</strong>
-        </span>
-        <span>5,000</span>
-      </li>
-      <li className={style.rankListCard}>
-        <strong>5</strong>
-        <span>
-          <img src="/img/test.jpg" alt="profile" />
-          <strong>닉네임</strong>
-        </span>
-        <span>5,000</span>
-      </li>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <li
+          key={i}
+          className={`${style.rankListCard} ${
+            hoveredCard === i ? style.hover : ''
+          }`}
+        >
+          <strong>{i}</strong>
+          <span>
+            <img src="/img/test.jpg" alt="profile" />
+            <strong>닉네임</strong>
+          </span>
+          <span>5,000</span>
+        </li>
+      ))}
     </ul>
   );
 };
