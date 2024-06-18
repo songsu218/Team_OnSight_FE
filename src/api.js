9d114249fbea60f2a52a1177c14302a1e5f161b4
songsu218 <chaengleee@naver.com>
2024-06-18 14:46:46 +0900

    feat : baseUrl 수정

--- src/api.js
+++ src/api.js
@@ -23,7 +23,7 @@ import axios from 'axios';
  * };
  */
 
-const baseURL = 'http://localhost:5000/api';
+const baseURL = 'http://localhost:8000';
 
 // axios 인스턴스를 생성합니다.
 const instance = axios.create({
