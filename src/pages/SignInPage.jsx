21a05d4c3eee4cf2236a4e4ecf07b1e66fa7e354
songsu218 <asane7@naver.com>
2024-06-22 13:34:29 +0900

    feat: 로그인부분 롤백

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import style from '../css/SignInPage.module.css';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import style from "../css/SignInPage.module.css";
+import { Link, Navigate } from "react-router-dom";
 
 const kakaoApiKey = process.env.REACT_APP_KAKAO_API_KEY;
 const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
@@ -8,31 +8,21 @@ const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 const link = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoApiKey}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 const SignInPage = () => {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [message1, setMessage1] = useState('');
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [message1, setMessage1] = useState("");
   const [redirect, setRedirect] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    console.log('Kakao API Key:', kakaoApiKey);
-    console.log('Redirect URI:', REDIRECT_URI);
-  }, []);
 
   const SignIn = async (e) => {
     e.preventDefault();
     console.log(id, password);
 
     try {
-      const token = JSON.parse(localStorage.getItem('token')).access_token;
       const response = await fetch(`http://localhost:8000/user/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ id, password }),
-        credentials: 'include',
+        credentials: "include",
       });
       const data = await response.json();
       console.log(data);
@@ -41,21 +31,17 @@ const SignInPage = () => {
       if (data.id) {
         setRedirect(true);
       }
-      if (data.message === 'nouser' || data.message === 'failed') {
-        setMessage1('아이디 또는 비밀번호가 맞지 않습니다.');
+      if (data.message === "nouser" || data.message === "failed") {
+        setMessage1("아이디 또는 비밀번호가 맞지 않습니다.");
       }
     } catch (error) {
-      console.error('Error:', error);
-      setMessage1('로그인 중 오류가 발생했습니다.');
+      console.error("Error:", error);
+      setMessage1("로그인 중 오류가 발생했습니다.");
     }
   };
 
-  const kakaoLoginHandler = () => {
-    window.location.href = link;
-  };
-
   if (redirect) {
-    navigate('/');
+    return <Navigate to="/" />;
   }
 
   return (
@@ -86,3 +72,45 @@ const SignInPage = () => {
 };
 
 export default SignInPage;
+
+// 로그인 부분 재수정해야해욧 20240622
+
+// useEffect(() => {
+//   console.log("Kakao API Key:", kakaoApiKey);
+//   console.log("Redirect URI:", REDIRECT_URI);
+// }, []);
+
+// const SignIn = async (e) => {
+//   e.preventDefault();
+//   console.log(id, password);
+
+//   try {
+//     const token = JSON.parse(localStorage.getItem('token')).access_token;
+//     const response = await fetch(`http://localhost:8000/user/login`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify({ id, password }),
+//       credentials: 'include',
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//     console.log(data.id);
+
+//     if (data.id) {
+//       setRedirect(true);
+//     }
+//     if (data.message === 'nouser' || data.message === 'failed') {
+//       setMessage1('아이디 또는 비밀번호가 맞지 않습니다.');
+//     }
+//   } catch (error) {
+//     console.error('Error:', error);
+//     setMessage1('로그인 중 오류가 발생했습니다.');
+//   }
+// };
+
+// const kakaoLoginHandler = () => {
+//   window.location.href = link;
+// };
