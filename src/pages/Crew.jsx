6fde504a35c84ef9ae9bfc426937a788c67fcb4b
kyuhwan <konge2963@gmail.com>
2024-06-22 15:52:07 +0900

    feat : 추천 크루 껍데기만 완성

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,100 +1,121 @@
-import style from "../css/Crew.module.css";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import style from '../css/Crew.module.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Crew = () => {
   // const nick = "춘식이";
 
   const districtsByRegion = {
     seoul: [
-      "종로구",
-      "중구",
-      "용산구",
-      "성동구",
-      "광진구",
-      "동대문구",
-      "중랑구",
-      "성북구",
-      "강북구",
-      "도봉구",
-      "노원구",
-      "은평구",
-      "서대문구",
-      "마포구",
-      "양천구",
-      "강서구",
-      "구로구",
-      "금천구",
-      "영등포구",
-      "동작구",
-      "관악구",
-      "서초구",
-      "강남구",
-      "송파구",
-      "강동구",
+      '종로구',
+      '중구',
+      '용산구',
+      '성동구',
+      '광진구',
+      '동대문구',
+      '중랑구',
+      '성북구',
+      '강북구',
+      '도봉구',
+      '노원구',
+      '은평구',
+      '서대문구',
+      '마포구',
+      '양천구',
+      '강서구',
+      '구로구',
+      '금천구',
+      '영등포구',
+      '동작구',
+      '관악구',
+      '서초구',
+      '강남구',
+      '송파구',
+      '강동구',
     ],
     busan: [
-      "중구",
-      "서구",
-      "동구",
-      "영도구",
-      "부산진구",
-      "동래구",
-      "남구",
-      "북구",
-      "해운대구",
-      "사하구",
-      "금정구",
-      "강서구",
-      "연제구",
-      "수영구",
-      "사상구",
-      "기장군",
+      '중구',
+      '서구',
+      '동구',
+      '영도구',
+      '부산진구',
+      '동래구',
+      '남구',
+      '북구',
+      '해운대구',
+      '사하구',
+      '금정구',
+      '강서구',
+      '연제구',
+      '수영구',
+      '사상구',
+      '기장군',
     ],
   };
 
-  const [selectedRegion, setSelectedRegion] = useState("");
-  const [selectedDistrict, setSelectedDistrict] = useState("");
+  const [selectedRegion, setSelectedRegion] = useState('');
+  const [selectedDistrict, setSelectedDistrict] = useState('');
+  const [searchCrew, setSearchCrew] = useState('');
 
   const handleRegionChange = (event) => {
     setSelectedRegion(event.target.value);
-    setSelectedDistrict(""); // Reset district selection when region changes
+    setSelectedDistrict(''); // Reset district selection when region changes
   };
 
   const handleDistrictChange = (event) => {
     setSelectedDistrict(event.target.value);
   };
 
+  const handleSearchChange = (e) => {
+    setSearchCrew(e.target.value);
+  };
+
+  const crewList = [
+    {
+      crewName: '영통 크루',
+      members: '1',
+      content: '크루 소개글12341234',
+    },
+    {
+      crewName: '강남 크루',
+      members: '2',
+      content: '크루 소개글56785678',
+    },
+    {
+      crewName: '북한 크루',
+      members: '3',
+      content: '크루 소개글12341234',
+    },
+    {
+      crewName: '미국 크루',
+      members: '4',
+      content: '크루 소개글56785678',
+    },
+  ];
+
+  const filteredCrewList = crewList.filter((crew) =>
+    crew.crewName.includes(searchCrew)
+  );
+
   return (
     <main className={`${style.mainCrew} viewCon`}>
       <article>
-        <div className={style.leftCon}>
-          <span>나의크루 리스트</span>
-          {/* <p>{nick}님이 가입된 크루가 없습니다.</p> */}
-          <ul className={style.mycrewCon}>
-            <li>
+        <div className={style.myCrewCon}>
+          <h3>나의크루 리스트</h3>
+          <ul className={style.myCrewListCon}>
+            <li className={style.crewListCard}>
               <Link to="/CrewDetail">
-                <img src="/img/imgsample.jpg" alt="img" />
-                <div className={style.mycrew}>
-                  <div className={style.adress}>
-                    <span>name</span>
-                    <p> gu구 </p>
+                <div className={style.crewImgBox}>
+                  <img src="/img/imgsample.png" alt="img" />
+                </div>
+                <div className={style.crewTextBox}>
+                  <div className={style.crewNameBox}>
+                    <strong>크루 이름</strong>
+                    <span> 영통구 </span>
                   </div>
-                  <p>content</p>
-                  <p>참가자수 : x명</p>
+                  <p>크루 소개글12341234</p>
                 </div>
               </Link>
-            </li>
-            <li>
-              <img src="/img/no_img.jpg" alt="img" />
-              <div className={style.mycrew}>
-                <div className={style.adress}>
-                  <span>name</span>
-                  <p> gu구 </p>
-                </div>
-                <p>content</p>
-                <p>참가자수 : x명</p>
-              </div>
             </li>
           </ul>
         </div>
@@ -103,14 +124,12 @@ const Crew = () => {
         <div className={style.righttxt}>
           <h2>추천크루</h2>
           <Link to="/CreateCrew">
-            <button>크루생성</button>
+            <button className={style.crewCreateBtn}>크루생성</button>
           </Link>
         </div>
         <div className={style.rightmenu}>
           <div className={style.selectBox}>
-            <label for="region-select">
-              <span>지역</span>
-            </label>
+            <span>지역</span>
             <select
               id="region-select"
               value={selectedRegion}
@@ -135,70 +154,30 @@ const Crew = () => {
                 ))}
             </select>
           </div>
-          <div>
+          <div className={style.searchBox}>
             <input
               type="text"
-              className={style.searchBox}
               placeholder="크루검색"
+              value={searchCrew}
+              onChange={handleSearchChange}
             />
-            <i class="fa-solid fa-magnifying-glass"></i>
+            <i className="fa-solid fa-magnifying-glass"></i>
           </div>
         </div>
 
         <ul className={style.crewList}>
-          <li>
-            <img src="/img/bol.jpg" alt="크루" />
-            <h5>크루명</h5>
-            <p>content</p>
-            <span>참가인원 : xx명</span>
-          </li>
-          <li>
-            <img src="/img/bol.jpg" alt="크루" />
-            <h5>크루명</h5>
-            <p>content</p>
-            <span>참가인원 : xx명</span>
-          </li>
-          <li>
-            <img src="/img/bol.jpg" alt="크루" />
-            <h5>크루명</h5>
-            <p>content</p>
-            <span>참가인원 : xx명</span>
-          </li>
-          <li>
-            <img src="/img/bol.jpg" alt="크루" />
-            <h5>크루명</h5>
-            <p>content</p>
-            <span>참가인원 : xx명</span>
-          </li>
-          <li>
-            <img src="/img/bol.jpg" alt="크루" />
-            <h5>크루명</h5>
-            <p>content</p>
-            <span>참가인원 : xx명</span>
-          </li>
-          <li>
-            <img src="/img/bol.jpg" alt="크루" />
-            <h5>크루명</h5>
-            <p>content</p>
-            <span>참가인원 : xx명</span>
-          </li>
-          <li>
-            <img src="/img/bol.jpg" alt="크루" />
-            <h5>크루명</h5>
-            <p>
-              content: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Ut nam facere officia doloribus eius, sit, dolorum delectus
-              ratione, repudiandae ad suscipit voluptatum. Tenetur rerum sunt
-              dicta voluptates temporibus officiis ducimus?
-            </p>
-            <span>참가인원 : xx명</span>
-          </li>
-          <li>
-            <img src="/img/bol.jpg" alt="크루" />
-            <h5>크루명</h5>
-            <p>content</p>
-            <span>참가인원 : xx명</span>
-          </li>
+          {filteredCrewList.map((crew) => (
+            <li key={crew} className={style.crewListCard}>
+              <div className={style.recCrewImgBox}>
+                <img src="/img/imgsample.png" alt="img" />
+              </div>
+              <div className={style.crewListTextBox}>
+                <strong>{crew.crewName}</strong>
+                <p>{crew.content}</p>
+                <span>크루원 : {crew.members}명</span>
+              </div>
+            </li>
+          ))}
         </ul>
       </section>
     </main>
