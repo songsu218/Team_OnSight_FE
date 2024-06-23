287e510f76f7d73c35f162d375b31b2b12677b5b
LeeYoungin09 <inleeyoung2@gmail.com>
2024-06-23 13:44:31 +0900

    feat:쓰기 페이지 수정

--- src/components/CrewWrite.jsx
+++ src/components/CrewWrite.jsx
@@ -8,55 +8,48 @@ const CrewWrite = () => {
   const [content, setContent] = useState("");
 
   return (
-    <div className="con1">
-      <main id={style.container} className="mw">
-        <div className={style.content}>
-          <div className={style.page_tit_area}>
-            <h2 className={style.page_tit}>피드 작성하기</h2>
-            {/* <form className={style.writeCon} onSubmit={updatePost}> */}
-            <form className={style.writeCon}>
-              <div className={style.titleCon}>
-                <label htmlFor="title">
-                  <h2>제목</h2>
-                </label>
-                <input
-                  type="text"
-                  name="title"
-                  id="title"
-                  placeholder="제목을 입력해주세요"
-                  value={title}
-                  // onChange={(e) => setTitle(e.target.value)}
-                />
-              </div>
-              <div className={style.contentCon}>
-                <label htmlFor="content">
-                  <h2>내용</h2>
-                </label>
-                <Editor content={content} setContent={setContent} />
-              </div>
-              <div className={style.fileCon}>
-                <label htmlFor="files">
-                  <h2>첨부파일</h2>
-                </label>
-                <input
-                  type="file"
-                  name="files"
-                  id="files"
-                  onChange={(e) => setFiles(e.target.files)}
-                />
-                <p className={style.smallImgCon}>
-                  {/* <img src={`${url}/${cover}`} alt={title} /> */}
-                </p>
-              </div>
-
-              <div className={style.buttonCon}>
-                <button>취소</button>
-                <button>작성하기</button>
-              </div>
-            </form>
-          </div>
+    <div>
+      {/* <form className={style.writeCon} onSubmit={updatePost}> */}
+      <form className={style.writeCon}>
+        <div className={style.titleCon}>
+          <label htmlFor="title">
+            <h2>제목</h2>
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="제목을 입력해주세요"
+            value={title}
+            // onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-      </main>
+        <div className={style.contentCon}>
+          <label htmlFor="content">
+            <h2>내용</h2>
+          </label>
+          <Editor content={content} setContent={setContent} />
+        </div>
+        <div className={style.fileCon}>
+          <label htmlFor="files">
+            <h2>첨부파일</h2>
+          </label>
+          <input
+            type="file"
+            name="files"
+            id="files"
+            onChange={(e) => setFiles(e.target.files)}
+          />
+          <p className={style.smallImgCon}>
+            {/* <img src={`${url}/${cover}`} alt={title} /> */}
+          </p>
+        </div>
+
+        <div className={style.buttonCon}>
+          <button>취소</button>
+          <button>작성하기</button>
+        </div>
+      </form>
     </div>
   );
 };
