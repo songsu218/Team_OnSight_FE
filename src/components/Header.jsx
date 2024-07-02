42e659fc40e9d88e3638dacc8cd4b7d895ee24c3
kyuhwan <konge2963@gmail.com>
2024-07-02 23:22:44 +0900

    feat : 햄버거 버튼 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
-import style from "../css/Header.module.css";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { clearUserAllInfo, setUserAllInfo } from "../store/userStore";
-import { persistor } from "../store/store";
-import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from 'react';
+import style from '../css/Header.module.css';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearUserAllInfo, setUserAllInfo } from '../store/userStore';
+import { persistor } from '../store/store';
+import { jwtDecode } from 'jwt-decode';
 
 const Header = () => {
   const user = useSelector((state) => state.user.userInfo);
@@ -16,6 +16,12 @@ const Header = () => {
     link5: false,
   });
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   const dispatch = useDispatch();
   const location = useLocation();
   const navigate = useNavigate();
@@ -23,21 +29,21 @@ const Header = () => {
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const token = localStorage.getItem("onSightToken");
+        const token = localStorage.getItem('onSightToken');
         if (token) {
           if (isTokenExpired(token)) {
             //토큰 유효기간 확인
-            localStorage.removeItem("onSightToken");
+            localStorage.removeItem('onSightToken');
             dispatch(clearUserAllInfo());
-            alert("로그인이 만료되었습니다. 다시 로그인 해주세요.");
-            navigate("/signinpage");
+            alert('로그인이 만료되었습니다. 다시 로그인 해주세요.');
+            navigate('/signinpage');
             return;
           }
           const response = await fetch(`http://localhost:8000/user/profile`, {
-            method: "POST",
+            method: 'POST',
             headers: {
               Authorization: `Bearer ${token}`,
-              "Content-Type": "application/json",
+              'Content-Type': 'application/json',
             },
           });
           if (response.ok) {
@@ -48,12 +54,12 @@ const Header = () => {
           }
         }
       } catch (error) {
-        console.error("error", error);
+        console.error('error', error);
         dispatch(clearUserAllInfo());
       }
     };
 
-    const token = localStorage.getItem("onSightToken");
+    const token = localStorage.getItem('onSightToken');
     if (!user && token) {
       fetchProfile();
     }
@@ -66,7 +72,7 @@ const Header = () => {
       const currentTime = Date.now() / 1000;
       return decoded.exp < currentTime;
     } catch (error) {
-      console.error("Error decoding token:", error);
+      console.error('Error decoding token:', error);
       return true;
     }
   };
@@ -79,20 +85,20 @@ const Header = () => {
     e.preventDefault();
     try {
       const response = await fetch(`http://localhost:8000/user/logout`, {
-        method: "POST",
+        method: 'POST',
       });
       if (response.ok) {
         dispatch(clearUserAllInfo());
-        localStorage.removeItem("onSightToken");
+        localStorage.removeItem('onSightToken');
         persistor.purge();
-        alert("로그아웃 되었어요");
-        navigate("/");
+        alert('로그아웃 되었어요');
+        navigate('/');
         // navigate("/login");
       } else {
-        console.error("fail", response.statusText);
+        console.error('fail', response.statusText);
       }
     } catch (error) {
-      console.error("error", error);
+      console.error('error', error);
     }
   };
 
@@ -115,69 +121,74 @@ const Header = () => {
           </div>
         </h1>
         <nav>
-          <div className={style.holdBar}>
+          <div className={`${style.holdBar} ${menuOpen ? style.showMenu : ''}`}>
             <Link
               to="/SearchPage"
-              onMouseEnter={() => MouseHover("link1")}
-              onMouseLeave={() => MouseLeave("link1")}
+              onClick={() => setMenuOpen(false)}
+              onMouseEnter={() => MouseHover('link1')}
+              onMouseLeave={() => MouseLeave('link1')}
             >
               <img
-                src={hoverImg.link1 ? "/img/eholdr.png" : "/img/holdr.png"}
+                src={hoverImg.link1 ? '/img/eholdr.png' : '/img/holdr.png'}
                 alt=""
               />
-              <span style={{ color: hoverImg.link1 ? "#FF454A" : "#FFFFFF" }}>
+              <span style={{ color: hoverImg.link1 ? '#FF454A' : '#FFFFFF' }}>
                 암장찾기
               </span>
             </Link>
             <Link
               to="/crew"
-              onMouseEnter={() => MouseHover("link2")}
-              onMouseLeave={() => MouseLeave("link2")}
+              onClick={() => setMenuOpen(false)}
+              onMouseEnter={() => MouseHover('link2')}
+              onMouseLeave={() => MouseLeave('link2')}
             >
               <img
-                src={hoverImg.link2 ? "/img/eholdb.png" : "/img/holdb.png"}
+                src={hoverImg.link2 ? '/img/eholdb.png' : '/img/holdb.png'}
                 alt=""
               />
-              <span style={{ color: hoverImg.link2 ? "#0295CF" : "#FFFFFF" }}>
+              <span style={{ color: hoverImg.link2 ? '#0295CF' : '#FFFFFF' }}>
                 크루
               </span>
             </Link>
             <Link
               to="/challenge"
-              onMouseEnter={() => MouseHover("link3")}
-              onMouseLeave={() => MouseLeave("link3")}
+              onClick={() => setMenuOpen(false)}
+              onMouseEnter={() => MouseHover('link3')}
+              onMouseLeave={() => MouseLeave('link3')}
             >
               <img
-                src={hoverImg.link3 ? "/img/eholdy.png" : "/img/holdy.png"}
+                src={hoverImg.link3 ? '/img/eholdy.png' : '/img/holdy.png'}
                 alt=""
               />
-              <span style={{ color: hoverImg.link3 ? "#FFD02C" : "#FFFFFF" }}>
+              <span style={{ color: hoverImg.link3 ? '#FFD02C' : '#FFFFFF' }}>
                 챌린지
               </span>
             </Link>
             <Link
               to="/rank"
-              onMouseEnter={() => MouseHover("link4")}
-              onMouseLeave={() => MouseLeave("link4")}
+              onClick={() => setMenuOpen(false)}
+              onMouseEnter={() => MouseHover('link4')}
+              onMouseLeave={() => MouseLeave('link4')}
             >
               <img
-                src={hoverImg.link4 ? "/img/eholdg.png" : "/img/holdg.png"}
+                src={hoverImg.link4 ? '/img/eholdg.png' : '/img/holdg.png'}
                 alt=""
               />
-              <span style={{ color: hoverImg.link4 ? "#A2D262" : "#FFFFFF" }}>
+              <span style={{ color: hoverImg.link4 ? '#A2D262' : '#FFFFFF' }}>
                 랭킹
               </span>
             </Link>
             <Link
-              to={`/mypage/home/${user?.id}`}
-              onMouseEnter={() => MouseHover("link5")}
-              onMouseLeave={() => MouseLeave("link5")}
+              to="/mypage"
+              onClick={() => setMenuOpen(false)}
+              onMouseEnter={() => MouseHover('link5')}
+              onMouseLeave={() => MouseLeave('link5')}
             >
               <img
-                src={hoverImg.link5 ? "/img/eholdp.png" : "/img/holdp.png"}
+                src={hoverImg.link5 ? '/img/eholdp.png' : '/img/holdp.png'}
                 alt=""
               />
-              <span style={{ color: hoverImg.link5 ? "#BE65FF" : "#FFFFFF" }}>
+              <span style={{ color: hoverImg.link5 ? '#BE65FF' : '#FFFFFF' }}>
                 MY
               </span>
             </Link>
@@ -186,17 +197,19 @@ const Header = () => {
       </div>
       <div className={style.viewMore}>
         {username ? (
-          <Link to="/" onClick={signout}>
+          <Link to="/">
             <i className="fa-solid fa-sign-out-alt"></i>
             <div className={style.nickBox}>
               <span>{nickname}님</span>
-              <span>로그아웃</span>
+              <span onClick={signout}>로그아웃</span>
+              <i className="fa-solid fa-bars" onClick={toggleMenu}></i>
             </div>
           </Link>
         ) : (
           <Link to="/signinpage">
             <i className="fa-solid fa-gear"></i>
             <span>로그인</span>
+            <i className="fa-solid fa-bars" onClick={toggleMenu}></i>
           </Link>
         )}
       </div>
